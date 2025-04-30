23fbab36fd1160d27706a3603e9b305b1d85a78c
MirDuran <mduranre@uoc.edu>
2025-04-30 12:55:08 +0200

    Dependencias Maven  y conexionHibernate

--- src/main/java/javalinos/onlinestore/modelo/ORM/ArticuloORM.java
+++ src/main/java/javalinos/onlinestore/modelo/ORM/ArticuloORM.java
@@ -1,4 +1,65 @@
 package javalinos.onlinestore.modelo.ORM;
 
+import javalinos.onlinestore.modelo.Entidades.Articulo;
+import javalinos.onlinestore.utils.Conexiones.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.util.List;
+
 public class ArticuloORM {
+
+    public static void insertarArticulo(Articulo articulo) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            session.persist(articulo); // También puedes usar session.save(articulo);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) transaction.rollback();
+            e.printStackTrace();
+        }
+    }
+
+    public static Articulo obtenerArticuloPorId(int id) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return session.get(Articulo.class, id);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static List<Articulo> obtenerTodosLosArticulos() {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return session.createQuery("from Articulo", Articulo.class).list();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static void actualizarArticulo(Articulo articulo) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            session.update(articulo);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) transaction.rollback();
+            e.printStackTrace();
+        }
+    }
+
+    public static void eliminarArticulo(Articulo articulo) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            session.delete(articulo);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) transaction.rollback();
+            e.printStackTrace();
+        }
+    }
 }
---
+++ src/main/java/javalinos/onlinestore/utils/Conexiones/HibernateUtil.java
@@ -0,0 +1,33 @@
+package javalinos.onlinestore.utils.Conexiones;
+import javalinos.onlinestore.Configuracion;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.service.ServiceRegistry;
+
+public class HibernateUtil {
+    private static final SessionFactory sessionFactory;
+
+    static {
+        try {
+            Configuration configuration = new Configuration().configure();
+
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties()).build();
+
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        } catch (Exception ex) {
+            System.err.println("Fallo al crear SessionFactory: " + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    public static void shutdown() {
+        getSessionFactory().close();
+    }
+}
