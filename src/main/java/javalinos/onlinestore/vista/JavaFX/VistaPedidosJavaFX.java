731fb76892556575b0370e548bf650b89cf5454b
MiguelGarzo <mgarzom@uoc.edu>
2025-05-19 18:51:41 +0200

    Pedidos con master

--- src/main/java/javalinos/onlinestore/vista/JavaFX/VistaPedidosJavaFX.java
+++ src/main/java/javalinos/onlinestore/vista/JavaFX/VistaPedidosJavaFX.java
@@ -1,16 +1,102 @@
 package javalinos.onlinestore.vista.JavaFX;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javalinos.onlinestore.controlador.ControlArticulos;
+import javalinos.onlinestore.controlador.ControlPedidos;
 import javalinos.onlinestore.modelo.DTO.ArticuloDTO;
 import javalinos.onlinestore.modelo.DTO.ClienteDTO;
 import javalinos.onlinestore.modelo.DTO.PedidoDTO;
 import javalinos.onlinestore.vista.Interfaces.IVistaPedidos;
 
+import java.net.URL;
+import java.time.LocalDate;
 import java.util.List;
+import java.util.ResourceBundle;
 
 public class VistaPedidosJavaFX extends VistaBaseJavaFX implements IVistaPedidos {
+    private ControlPedidos cPedidos;
+
+    @FXML
+    private TableView<PedidoDTO> tablaPedidos;
+
+    @FXML
+    private TableColumn<PedidoDTO, Integer> columnaNumero;
+    @FXML
+    private TableColumn<PedidoDTO, ClienteDTO> columnaCliente;
+    @FXML
+    private TableColumn<PedidoDTO, String> columnaArticulo;
+    @FXML
+    private TableColumn<PedidoDTO, Integer> columnaCantidad;
+    @FXML
+    private TableColumn<PedidoDTO, LocalDate> columnaFecha;
+    @FXML
+    private TableColumn<PedidoDTO, Float> columnaEnvio;
+    @FXML
+    private TableColumn<PedidoDTO, Float> columnaPrecio;
+
+    @FXML
+    private Button botonNuevoPedido;
+    @FXML
+    private Button botonEliminarPedido;
+    @FXML
+    private Button botonEditarPedido;
+    @FXML
+    private Button botonListarTodosPedidos;
+    @FXML
+    private Button botonListarPedidosPendientes;
+    @FXML
+    private Button botonListarPedidosEnviados;
+
+    public VistaPedidosJavaFX(ControlArticulos cArticulos) {
+        this.cPedidos = cPedidos;
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        columnaNumero.setCellValueFactory(new PropertyValueFactory<>("numero"));
+        columnaCliente.setCellValueFactory(new PropertyValueFactory<>("cliente"));
+        columnaArticulo.setCellValueFactory(new PropertyValueFactory<>("articulo"));
+        columnaCantidad.setCellValueFactory(new PropertyValueFactory<>("cantidad"));
+        columnaFecha.setCellValueFactory(new PropertyValueFactory<>("fechahora"));
+        columnaEnvio.setCellValueFactory(new PropertyValueFactory<>("envio"));
+        columnaPrecio.setCellValueFactory(new PropertyValueFactory<>("precioTotal"));
+
+        botonNuevoPedido.setOnAction(event -> addPedido());
+        botonEliminarPedido.setOnAction(event -> removePedido());
+        botonEditarPedido.setOnAction(event -> modPedido());
+        botonListarTodosPedidos.setOnAction(event -> listarPedido(null));
+        botonListarPedidosEnviados.setOnAction(event -> showPedidosEnviados(null, true));
+        botonListarPedidosPendientes.setOnAction(event -> showPedidosEnviados(null, false));
+    }
+
+    private void addPedido() { cPedidos.addPedidos(); }
+
+    @FXML
+    private void removePedido() { cPedidos.removePedidos(); }
+
+    private void modPedido() {
+        cPedidos.updatePedido();
+    }
+
+    private void listarPedido(ClienteDTO clienteDTO) {
+        tablaPedidos.getItems().clear();
+        cPedidos.showListPedidos(clienteDTO);
+    }
+
+    private void showPedidosEnviados(ClienteDTO clienteDTO, boolean enviado){
+        tablaPedidos.getItems().clear();
+        cPedidos.showListPedidosPendientesEnviados(clienteDTO, enviado);
+    }
+
     @Override
     public void showListPedidos(List<PedidoDTO> pedidosDTO, ClienteDTO clienteDTO, boolean opcion) {
-
+        tablaPedidos.getItems().clear();
+        tablaPedidos.getItems().addAll(pedidosDTO);
     }
 
     @Override
@@ -56,5 +142,13 @@ public class VistaPedidosJavaFX extends VistaBaseJavaFX implements IVistaPedidos
     @Override
     public ClienteDTO askClienteOpcional(List<ClienteDTO> clientesDTO, ClienteDTO clienteDTOActual) {
         return null;
+    }
+
+    public void mostrarAlerta(Alert.AlertType tipo, String titulo, String encabezado, String contenido) {
+        Alert alert = new Alert(tipo);
+        alert.setTitle(titulo);
+        alert.setHeaderText(encabezado);
+        alert.setContentText(contenido);
+        alert.showAndWait();
     }
 }
